e39f5241cea773ae3580d313910ba4ae780f8c0d
bugficks <2992895+bugficks@users.noreply.github.com>
2024-12-18 08:35:26 +0700

    update deps

--- omramin.py
+++ omramin.py
@@ -12,7 +12,6 @@ import logging.config
 import os
 import pathlib
 from datetime import datetime, timedelta
-from packaging.version import Version
 
 import bleak
 import click
@@ -27,10 +26,6 @@ from regionserver import get_server_for_country_code
 ########################################################################################################################
 
 __version__ = "0.1.1"
-
-########################################################################################################################
-# https://github.com/matin/garth/issues/73#issuecomment-2500754984
-GC.garth.http.USER_AGENT = {"User-Agent": ("GCM-iOS-5.7.2.1")}
 
 ########################################################################################################################
 
